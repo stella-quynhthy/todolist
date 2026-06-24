bfbdc2cdb3865896f1c1140e4b57f23e7d451ac6
stella-quynhthy <stellaqtnguyen@gmail.com>
2026-06-24 18:31:20 -0400

    updated

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-const listContainer = document.getElementById("list");
-const addBtn = document.getElementById("addButton");
-const inputField = document.getElementById("addTask");
+document.addEventListener("DOMContentLoaded", function() {
+    const listContainer = document.getElementById("list");
+    const addBtn = document.getElementById("addButton");
+    const inputField = document.getElementById("addTask");
 
-addBtn.addEventListener("click", () => {
-    const task = inputField.value.trim();
-    const listItem = document.createElement("li");
-    listItem.textContent = task;
-    listContainer.appendChild(listItem);
+    function addLi() {
+        const task = inputField.value.trim();
+        const listItem = document.createElement("li");
+        listItem.textContent = task;
+        listContainer.appendChild(listItem);
+    }
+
+    addBtn.addEventListener("click", addLi);
+    addBtn.addEventListener("keydown", function(e) {
+        if (e.key === "Enter") {
+            e.preventDefault();
+            addLi();
+        }
+    });
 });
